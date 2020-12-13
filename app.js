f15a376cad0fc71b8b5cee1a11d3cd046d9ba02f
bgubanov <b.gubanov@list.ru>
2020-12-13 17:02:27 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 const request = require('request');
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
