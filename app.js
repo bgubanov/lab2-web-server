02d5c3e98a4bc12c1e56e4158ab2ac668a829982
bgubanov <b.gubanov@list.ru>
2020-12-13 16:47:20 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 8080
 const request = require('request');
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
